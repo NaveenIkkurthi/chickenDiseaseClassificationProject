b278b45fbd52f991e31be33a1a23c6c2a1e3dad6
Naveen Ikkurthi <naveenikkurthi23@gmail.com>
2024-07-17 03:51:01 -0500

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
@@ -10,3 +11,19 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME = 'Prepare base model'
+
+if __name__ == '__main__':
+    try:
+
+        logger.info(f'Starting {STAGE_NAME} pipeline')
+        pipeline = PrepareBaseModelTrainingPipeline()
+        pipeline.main()
+        logger.info(f'Completed {STAGE_NAME} pipeline')
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,33 @@
+from cnnClassifier.config.configuaration import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier import logger
+
+
+STAGE_NAME = 'Prepare base model'
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+
+        logger.info(f'Starting {STAGE_NAME} pipeline')
+        pipeline = PrepareBaseModelTrainingPipeline()
+        pipeline.main()
+        logger.info(f'Completed {STAGE_NAME} pipeline')
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
